89ef9b9d901142c35f01788c3809f5bd67877d53
Linus Unnebäck <linus@folkdatorn.se>
2017-11-01 13:00:15 +0000

    🌹 Add comments to different code point groups

--- index.js
+++ index.js
@@ -17,23 +17,38 @@ module.exports = function encodeUtf8 (input) {
       }
     }
 
+    // US-ASCII
     if (point < 128) {
       result.push(point)
-    } else if (point < 2048) {
+      continue
+    }
+
+    // 2-byte UTF-8
+    if (point < 2048) {
       result.push((point >> 6) | 192)
       result.push((point & 63) | 128)
-    } else if (point < 65536) {
+      continue
+    }
+
+    // 3-byte UTF-8
+    if (point < 65536) {
       result.push((point >> 12) | 224)
       result.push(((point >> 6) & 63) | 128)
       result.push((point & 63) | 128)
-    } else if (point < 2097152) {
+      continue
+    }
+
+    // 4-byte UTF-8
+    if (point < 2097152) {
       result.push((point >> 18) | 240)
       result.push(((point >> 12) & 63) | 128)
       result.push(((point >> 6) & 63) | 128)
       result.push((point & 63) | 128)
-    } else {
-      throw new Error(`Code point ${point} cannot be encoded with UTF8`)
+      continue
     }
+
+    // Invalid character
+    throw new Error(`Code point ${point} cannot be encoded with UTF8`)
   }
 
   return Uint8Array.from(result).buffer
